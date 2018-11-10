20f2591db386bec4692d751558b12e246c41dfba
Andrewpqc <m13636038496@163.com>
2018-11-10 15:34:58 +0800

    update

---
+++ chapter20_signal_basic_concept/scan_process_id.c
@@ -0,0 +1,25 @@
+#include <signal.h>
+#include "../lib/tlpi_hdr.h"
+
+int main(int argc, char** argv){
+
+    static size_t  process_count =0;
+    for(pid_t i=1;i<50000;++i) {
+        ssize_t res = kill(i,0);
+        if(res==0){
+            printf("process %d exist, have right to send signal\n",i);
+            ++process_count;
+        } else if (res==-1){
+            if(errno==EPERM) {
+                printf("process %d exist, no right to send signal\n", i);
+                ++process_count;
+            }
+            else if(errno==ESRCH) {
+//                printf("process %d not exist.\n",i);
+            }
+        }
+    }
+
+    printf("%lu process in the system\n",process_count);
+    return 0;
+}
