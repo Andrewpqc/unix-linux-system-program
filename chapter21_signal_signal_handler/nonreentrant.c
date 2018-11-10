41426c859118e27d18064409d21f2ffd79850ea3
Andrewpqc <m13636038496@163.com>
2018-11-10 20:10:25 +0800

    add

---
+++ chapter21_signal_signal_handler/nonreentrant.c
@@ -0,0 +1,43 @@
+#define _XOPEN_SOURCE 600
+
+#include <signal.h>
+#include <unistd.h>
+#include <string.h>
+//#include <crypt.h>
+#include "../lib/tlpi_hdr.h"
+
+static char *str2;
+static int handled = 0;
+
+void sigint_handler(int sig) {
+    crypt(str2, "xx");
+}
+
+int main(int argc, char **argv) {
+    if (argc != 3 || strcmp(argv[1], "--help") == 0)
+        usageErr("%s str1 str2\n", argv[0]);
+
+    /* register SIGINT handler with sigaction */
+    struct sigaction sa;
+    sigemptyset(&sa.sa_mask);
+    sa.sa_flags = 0;
+    sa.sa_handler = sigint_handler;
+    if (sigaction(SIGINT, &sa, NULL) == -1)
+        errExit("sigaction");
+
+    str2 = argv[2];
+    char *encodedStr1 = strdup(crypt(argv[1], "xx")); // store to encodedStr1
+
+    for (int i = 0;; ++i) {
+        if (strcmp(crypt(argv[1], "xx"), encodedStr1) != 0) {
+            printf("mismatched\n");
+            errExit("mismatched");
+        }
+
+        printf("%d\n", i);
+
+    }
+
+
+    return 0;
+}
